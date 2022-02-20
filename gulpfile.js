491c9c531fa52f0f98906b8aa2c5c6b92653782d
Edison-vera <edisonefvc15@gmail.com>
2022-02-20 18:07:40 -0500

    Terser

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,14 @@
 // }
 
 // exports.tarea2 = tarea2;
-
-// CSS
 const { src, dest, watch, parallel } = require("gulp");
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
-
-
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 //Imagenes
 const cache = require("gulp-cache");
@@ -23,16 +24,20 @@ const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+//JS
+const terser = require('gulp-terser-js');
 
 function css(done) {
     //Identificar el archivo .SCSS a compilar 
     src("src/scss/**/*.scss")
+        .pipe(sourcemaps.init())
         //Compilarlo
         .pipe(plumber())
         .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write("."))
         //Almacenarlo
         .pipe(dest("build/css"))
-        .pipe()
     done();
 }
 
@@ -68,6 +73,9 @@ function versionAvif(done) {
 
 function javascript(done) {
     src("src/js/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write("."))
         .pipe(dest("build/js"))
     done();
 }
